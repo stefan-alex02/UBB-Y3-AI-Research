e85d8fa80b4913bc1206b9f3e70ade24456ffe2b
Ştefan Alexandrescu <stefan.alexandrescu2002@gmail.com>
2024-12-12 16:53:22 +0200

    Code refactorization for ViT classifier

--- models/data_splitter.py
+++ models/data_splitter.py
@@ -5,10 +5,12 @@ directory = 'mini-GCD/'
 modified_directory = directory[:-1] + '-modified/'
 
 # Load the data from train and test folders into a common class dictionary
-def load_data():
+def load_data(only_include_classes=None):
     data = {}
     for folder in ['train', 'test']:
         for class_name in os.listdir(directory + folder):
+            if only_include_classes and class_name not in only_include_classes:
+                continue
             if class_name not in data:
                 data[class_name] = []
             for file in os.listdir(directory + folder + '/' + class_name):
@@ -17,10 +19,14 @@ def load_data():
     return data
 
 # Split the data into train, test and validation sets
-def split_data(data, train_size=0.8, test_size=0.1):
+def split_data(data, train_size=0.8, test_size=0.1, limit_ratio=None):
     train_data = {}
     test_data = {}
     val_data = {}
+
+    if limit_ratio:
+        for class_name, files in data.items():
+            data[class_name] = files[:int(len(files) * limit_ratio)]
 
     for class_name, files in data.items():
         train_data[class_name] = files[:int(len(files) * train_size)]
@@ -28,6 +34,10 @@ def split_data(data, train_size=0.8, test_size=0.1):
         val_data[class_name] = files[int(len(files) * (train_size + test_size)):]
 
     return train_data, test_data, val_data
+
+def delete_directory(directory):
+    if os.path.exists(directory):
+        shutil.rmtree(directory)
 
 # Save the data into train, test and validation folders
 def save_data(data: dict[str, list[str]], folder: str):
@@ -37,11 +47,16 @@ def save_data(data: dict[str, list[str]], folder: str):
         for file in files:
             shutil.copy(file, modified_directory + folder + '/' + class_name + '/')
 
+mini_dataset_classes = ['1_cumulus', '4_clearsky', '6_cumulonimbus']
+
 # Load the data
-data = load_data()
+data = load_data(only_include_classes=mini_dataset_classes)
 
 # Split the data
-train_data, test_data, val_data = split_data(data)
+train_data, test_data, val_data = split_data(data, limit_ratio=0.1)
+
+# Delete the existing modified directory
+delete_directory(modified_directory)
 
 # Save the data
 os.makedirs(modified_directory, exist_ok=True)
