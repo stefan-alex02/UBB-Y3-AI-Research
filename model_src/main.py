6620317e1e81864a06618aaf29396249f5f998f5
Ştefan Alexandrescu <43996943+stefan-alex02@users.noreply.github.com>
2025-06-19 18:18:24 +0300

    Exp

--- model_src/main.py
+++ model_src/main.py
@@ -42,7 +42,7 @@ if __name__ == "__main__":
 
     # --- Configuration ---
     # Select Dataset:
-    selected_dataset = "swimcat"  # 'GCD', 'GCDf', 'mGCD', 'mGCDf', 'swimcat', 'ccsn'
+    selected_dataset = "GCD"  # 'GCD', 'GCDf', 'mGCD', 'mGCDf', 'swimcat', 'ccsn'
 
     # Select Model:
     model_type = "cloudnet"
@@ -60,7 +60,7 @@ if __name__ == "__main__":
     # 5: Non-Nested Grid Search + CV Evaluation (Requires FLAT or FIXED with force_flat=True)
     # 6: Load Pre-trained and Evaluate
     # 7: Load Pre-trained and Predict on New Images
-    chosen_sequence_idx = 4
+    chosen_sequence_idx = 1
 
     # Image size for the model
     img_size = (224, 224)
